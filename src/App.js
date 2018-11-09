2dc65e3c5406324dc6d293c83410eb38c92fb7ce
Manny <coachmannytw@gmail.com>
2018-11-09 10:48:13 -0700

    added table

--- src/App.js
+++ src/App.js
@@ -7,16 +7,47 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-          poker
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            Learn React
+            Learn React poker
+
           </a>
         </header>
+        <div class="main">
+  <div class="table">
+    <div class="board">
+      <div class="card-small">
+        <p class="card-text black">A</p>
+        <p class="card-img black">&clubs;</p>
+      </div>
+      <div class="card-small">
+        <p class="card-text black">10</p>
+        <p class="card-img black">&spades;</p>
+      </div>
+      <div class="card-small">
+        <p class="card-text red">K</p>
+        <p class="card-img red">&hearts;</p>
+      </div>
+      <div class="card-small">
+        <p class="card-text red">Q</p>
+        <p class="card-img red">&diams;</p>
+      </div>
+      <div class="card-small">
+        <p class="card-text red">2</p>
+        <p class="card-img red">&diams;</p>
+      </div>
+    </div>
+  </div>
+</div>
+
       </div>
     );
   }
